8ae9f7b5abbdeb64bf50f69d83a4ac48ca7609cc
yunuscelikk <yunusnotfound@icloud.com>
2026-01-11 00:07:29 +0300

    Add constants for API endpoints

---
+++ eventgate_mobile/lib/core/constants.dart
@@ -0,0 +1,15 @@
+class AppConstants {
+  static const String baseUrl = "http://192.168.1.227:3000/api";
+
+  static const String loginEndpoint = "/auth/login";
+
+  static const String eventsEndpoint = "/events";
+
+  static const String participantsEndpoint = "/participants";
+  static const String checkInEndpoint = "/participants/check-in";
+
+  static const String authTokenKey = "auth_token";
+
+  static const int connectionTimeout = 10000;
+  static const int receiveTimeout = 10000;
+}
