15e5e57b1f4ac33bda1db592d1224e5c4984d0d1
Yunus <yunusnotfound@icloud.com>
2026-01-17 02:38:07 +0300

    participant controller updated

--- server/src/controllers/eventController.js
+++ server/src/controllers/eventController.js
@@ -1,91 +1,97 @@
-const { Event, Sequelize } = require('../../models');
+const { Event, Sequelize } = require("../../models");
 const { Participant } = require("../../models");
 
 const getAllEvents = async (req, res) => {
-    try {
-        const events = await Event.findAll({
-            order: [['event_date', 'ASC']]
-        });
-        res.status(200).json(events);
-    } catch (err) {
-        res.status(500).json({error: "Events cant fetch"});
-    }
+  try {
+    const events = await Event.findAll({
+      order: [["event_date", "ASC"]],
+    });
+    res.status(200).json(events);
+  } catch (err) {
+    res.status(500).json({ error: "Events cant fetch" });
+  }
 };
 
 const getEventById = async (req, res) => {
-    const { eventId } = req.params
-    try {
-        const event = await Event.findByPk(eventId);
-        
-        if(!event) {
-            return res.status(404).json({error: "Event not found"});
-        }
+  const { eventId } = req.params;
+  try {
+    const event = await Event.findByPk(eventId);
 
-        res.status(200).json(event);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({error: "Server error"});
+    if (!event) {
+      return res.status(404).json({ error: "Event not found" });
     }
+
+    res.status(200).json(event);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
 };
 
 const createEvent = async (req, res) => {
-    const {title, description, event_date, location, image_url} = req.body;
-    try {
-        const event = await Event.create({
-            title,
-            description,
-            event_date,
-            location,
-            image_url
-        });
-        res.status(201).json(event);
-    } catch (err) {
-        console.error(err)
-        res.status(500).json({error: "Event creation failed"})
-    }
-}
+  const { title, description, event_date, location, image_url } = req.body;
+  try {
+    const event = await Event.create({
+      title,
+      description,
+      event_date,
+      location,
+      image_url,
+    });
+    res.status(201).json(event);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Event creation failed" });
+  }
+};
 const deleteEvent = async (req, res) => {
-     const { id } = req.params;
-     try {
-        const event = await Event.findByPk(id);
-        if (!event) {
-            return res.status(404).json({error: "Event not found"});
-        }
+  const { id } = req.params;
+  try {
+    const event = await Event.findByPk(id);
+    if (!event) {
+      return res.status(404).json({ error: "Event not found" });
+    }
 
-        await event.destroy();
+    await event.destroy();
 
-        res.status(200).json({
-            message: "Event deleted successfully",
-            deletedEvent: event
-        });
-     } catch (err) {
-        console.error(err);
-        res.status(500).json({error: "Server error during deletion"});
-     }
+    res.status(200).json({
+      message: "Event deleted successfully",
+      deletedEvent: event,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error during deletion" });
+  }
 };
 
 const updateEvent = async (req, res) => {
-    const { id } = req.params
-    const {title, description, event_date, location, image_url} = req.body
-    try {
-        const event = await Event.findByPk(id);
+  const { id } = req.params;
+  const { title, description, event_date, location, image_url } = req.body;
+  try {
+    const event = await Event.findByPk(id);
 
-        if (!event) {
-            return res.status(404).json({error: "Event not found"});
-        }
-
-        await event.update({
-            title,
-            description,
-            event_date,
-            location,
-            image_url
-        });
-        res.status(200).json(event);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({error: "Update failed"});
+    if (!event) {
+      return res.status(404).json({ error: "Event not found" });
     }
+
+    await event.update({
+      title,
+      description,
+      event_date,
+      location,
+      image_url,
+    });
+    res.status(200).json(event);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Update failed" });
+  }
 };
 
-module.exports = { getAllEvents, getEventById, createEvent, deleteEvent, updateEvent };
+module.exports = {
+  getAllEvents,
+  getEventById,
+  createEvent,
+  deleteEvent,
+  updateEvent,
+};
--- server/src/controllers/participantController.js
+++ server/src/controllers/participantController.js
@@ -1,93 +1,117 @@
 const sequelize = require("../config/db");
 const { Participant } = require("../../models");
-const QRCode = require('qrcode');
-const { randomUUID: uuidv4 } = require('crypto');
+const QRCode = require("qrcode");
+const { randomUUID: uuidv4 } = require("crypto");
+
+const isValidUUID = (str) => {
+  const regex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  return regex.test(str);
+};
 
 const createParticipant = async (req, res) => {
-    try {
-        const { event_id, full_name, email } = req.body;
-        if (!event_id || !full_name || !email) {
-            return res.status(400).json({error: "All fields are required"});
-        }
-
-        const uniqueQrCode = uuidv4();
-
-        const newParticipant = await Participant.create({
-            event_id,
-            full_name,
-            email,
-            qr_code: uniqueQrCode,
-            checked_in_at: null
-        });
-
-        const qrImage = await QRCode.toDataURL(uniqueQrCode);
-
-        res.status(201).json({
-            message: "Participant created successfully",
-            data: newParticipant,
-            qr_image: qrImage
-        });
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({error: "Could not create participant"});
+  try {
+    const { event_id, full_name, email } = req.body;
+    if (!event_id || !full_name || !email) {
+      return res.status(400).json({ error: "All fields are required" });
     }
+
+    const uniqueQrCode = uuidv4();
+
+    const newParticipant = await Participant.create({
+      event_id,
+      full_name,
+      email,
+      qr_code: uniqueQrCode,
+      checked_in_at: null,
+    });
+
+    const qrImage = await QRCode.toDataURL(uniqueQrCode);
+
+    res.status(201).json({
+      message: "Participant created successfully",
+      data: newParticipant,
+      qr_image: qrImage,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Could not create participant" });
+  }
 };
+
 const checkInParticipant = async (req, res) => {
+  try {
     const { qr_code, event_id } = req.body;
 
-    try {
-        const participant = await Participant.findOne({
-            where: { qr_code: qr_code}
-        });
+    console.log("Gelen QR Kodu:", qr_code);
 
-        if (!participant) {
-            return res.status(404).json({error: "Invalid QR Code / Participant not found"});
-        }
-
-        if (participant.event_id != event_id) {
-            return res.status(403).json({error: "This ticket is for a different event!"});
-        }
-
-        if (participant.checked_in_at !== null) {
-            return res.status(409).json({
-                error: "Participant already checked in!",
-                check_in_time: participant.checked_in_at
-            });
-        }
-
-        participant.checked_in_at = new Date();
-        await participant.save();
-
-        res.status(200).json({
-            message: "Check-in successful",
-            participant: participant
-        });
-    } catch (err) {
-        console.log(err),
-        res.status(500).json({error: "Check-in process failed"});
+    if (!qr_code || !event_id) {
+      return res
+        .status(400)
+        .json({ error: "QR Code and Event ID are required" });
     }
+    if (!isValidUUID(qr_code)) {
+      console.log("Format Hatalı: UUID değil.");
+      return res.status(400).json({
+        error: "Geçersiz QR Formatı! (Sistemimize ait bir bilet değil)",
+      });
+    }
+    const participant = await Participant.findOne({
+      where: { qr_code: qr_code },
+    });
+
+    if (!participant) {
+      return res.status(404).json({ error: "Bilet bulunamadı!" });
+    }
+
+    if (participant.event_id != event_id) {
+      return res
+        .status(403)
+        .json({ error: "Bu bilet başka bir etkinliğe ait!" });
+    }
+
+    if (participant.checked_in_at !== null) {
+      return res.status(409).json({
+        error: "Bu bilet zaten kullanılmış!",
+        check_in_time: participant.checked_in_at,
+      });
+    }
+
+    participant.checked_in_at = new Date();
+    await participant.save();
+
+    res.status(200).json({
+      message: "Giriş Başarılı ✅",
+      participant: participant,
+    });
+  } catch (err) {
+    console.error("Check-in Error Detayı:", err); // Hatayı detaylı görelim
+    res.status(500).json({ error: "Check-in işlemi sırasında sunucu hatası." });
+  }
 };
 
 const getParticipantsByEvent = async (req, res) => {
-    const { eventId } = req.params;
-    try {
-        const participants = await Participant.findAll({
-            where: {
-                event_id: eventId
-            }
-        });
-        if (participants.length === 0) {
-            return res.status(404).json({
-                error: "No participants found for this event"
-            });
-        }
-        res.status(200).json(participants)
-        
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({error: "Could not fetch participants"});
+  const { eventId } = req.params;
+  try {
+    const participants = await Participant.findAll({
+      where: {
+        event_id: eventId,
+      },
+    });
+    if (participants.length === 0) {
+      return res.status(404).json({
+        error: "No participants found for this event",
+      });
     }
+    res.status(200).json(participants);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Could not fetch participants" });
+  }
 };
 
-
-module.exports = { createParticipant, checkInParticipant, getParticipantsByEvent }
+module.exports = {
+  createParticipant,
+  checkInParticipant,
+  getParticipantsByEvent,
+};
